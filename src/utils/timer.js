865ede5f42e8017d95c3b26b5171969ca07f3578
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-01-24 00:41:04 +0100

    Add timer functions

---
+++ src/utils/timer.js
@@ -0,0 +1,11 @@
+export function formatTimer (timerDuration) {
+    console.log(timerDuration);
+    if (!timerDuration) return null;
+    const timerDurationInMinutes = timerDuration / 60000 >> 0;
+    const timerDurationInHours = timerDurationInMinutes / 60 >> 0;
+    return `${addZero(timerDurationInHours)}:${addZero(timerDurationInMinutes % 60)}`;
+}
+
+function addZero (n) {
+    return n < 10 ? '0' + n : n;
+}
