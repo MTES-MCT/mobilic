13dd1ac6e112f4d63080dbaf386676b9cca3c638
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-01-25 23:30:33 +0100

    Use material-ui components everywhere + remove non-universal css

--- src/components/TimeOfService.js
+++ src/components/TimeOfService.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {formatTimer} from "../utils/time";
+import Typography from "@material-ui/core/Typography";
 
 
 export function TimeOfService ({ timer }) {
     return (
         <div className="time-of-service-container">
-            <h2>Temps de service : </h2>
-            <h2>{formatTimer(timer)}</h2>
+            <Typography variant="h6">Temps de service  :  {formatTimer(timer)}</Typography>
         </div>
     )
 }
