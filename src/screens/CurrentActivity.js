d8fcadad7399734f57ec70d32a76a36b8955bfa3
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-01-26 23:36:01 +0100

    Add information about team count on main screen

--- src/screens/CurrentActivity.js
+++ src/screens/CurrentActivity.js
@@ -3,17 +3,30 @@ import {TimeLine} from "../components/Timeline";
 import {TimeOfService} from "../components/TimeOfService";
 import {ActivitySwitchGrid} from "../components/ActivitySwitch";
 import Container from "@material-ui/core/Container";
+import Link from '@material-ui/core/Link';
 
 
-export function CurrentActivity ({ timers, currentActivity, currentDayEvents, pushNewCurrentDayEvent }) {
+export function CurrentActivity ({ timers, currentActivityName, currentDayEvents, pushNewCurrentDayEvent, teamMates, setOpenTeamSelectionModal }) {
     return (
         <Container className="container">
             <TimeLine dayEvents={currentDayEvents}/>
             <div style={{flexGrow: 1}} />
+            {teamMates.length > 0 &&
+                <Link
+                    className="current-team-summary"
+                    href="#"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setOpenTeamSelectionModal(true);
+                    }}
+                >
+                    Coéquipiers : {teamMates.length}
+                </Link>
+            }
             <TimeOfService timer={timers["total"]} />
             <ActivitySwitchGrid
                 timers={timers}
-                activityOnFocus={currentActivity}
+                activityOnFocus={currentActivityName}
                 pushActivitySwitchEvent={pushNewCurrentDayEvent}
             />
         </Container>
