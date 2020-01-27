c9f5876a0fb6de473f17bbe9c3307ea92b668f2c
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-01-27 19:36:37 +0100

    Add logic for exporting work logs as csv

--- src/utils/events.js
+++ src/utils/events.js
@@ -1,4 +1,8 @@
 import {ACTIVITIES} from "./activities";
+import stringify from "csv-stringify/lib/sync";
+import {formatDay, MILLISECONDS_IN_A_DAY} from "./time";
+import {share} from "./share";
+
 
 export function groupEventsByDay (events) {
     const eventsByDay = [[]];
@@ -11,4 +15,28 @@ export function groupEventsByDay (events) {
         }
     });
     return eventsByDay;
+}
+
+export function preFormatEvents (events) {
+    return events.map((event) => {
+        const utcDateString = new Date(event.date).toISOString();
+        return ({
+            day: utcDateString.slice(0, 10),
+            time: utcDateString.slice(11, 16),
+            activity: event.activityName,
+            team: event.team
+        })
+    });
+}
+
+export function shareEvents (events, stringifyFunc=stringify) {
+    const firstDay = events[0].date;
+    const lastDay = events[events.length - 1].date;
+    const areAllEventsOnTheSameDay = (lastDay - firstDay) < MILLISECONDS_IN_A_DAY;
+
+    const title = `Temps de travail du ${formatDay(firstDay)}${(areAllEventsOnTheSameDay ? "" : " au " + formatDay(lastDay))}`
+    const text = stringifyFunc(
+        preFormatEvents(events)
+    );
+    share(text, title);
 }
---
+++ src/utils/share.js
@@ -0,0 +1,36 @@
+function urlWithQueryString  (url, queryParams) {
+  if (!queryParams || queryParams.length === 0) {
+    return url;
+  }
+  const queryString = Object.keys(queryParams).map((key) => {
+    const encodedKey = encodeURIComponent(key);
+    const value = queryParams[key];
+    if (Array.isArray(value)) {
+      return value.map(v => `${encodedKey}=${encodeURIComponent(v)}`).join(
+        "&"
+      );
+    } else if (value === null || value === undefined) {
+      return `${encodedKey}=`;
+    } else {
+      return `${encodedKey}=${encodeURIComponent(value)}`;
+    }
+  }).join("&");
+  return url + "?" + queryString;
+}
+
+export function share (text, title, mailto="") {
+    if (navigator.share) {
+        return navigator.share({text, title});
+    }
+    else {
+        const mailWindow = window.open(urlWithQueryString(
+            `mailto:${mailto}`,
+            {
+                subject: title,
+                body: `Bonjour,\n\nVoici mon relevé de temps de travail : \n\n${text}`
+            }
+        ),
+            "_parent",
+        );
+    }
+}
