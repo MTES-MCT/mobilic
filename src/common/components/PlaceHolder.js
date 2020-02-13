45a813c1c8ead1c1f1b1a6e4f76f4fd096cc7066
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-02-13 01:04:25 +0100

    Style login and signup screens

--- src/common/components/PlaceHolder.js
+++ src/common/components/PlaceHolder.js
@@ -1,8 +1,15 @@
 import React from "react";
 
-export function PlaceHolder({ children }) {
+export function PlaceHolder({ children, style }) {
   return (
-    <div style={{ flexGrow: 1, display: "flex", flexDirection: "column" }}>
+    <div
+      style={{
+        flexGrow: 1,
+        display: "flex",
+        flexDirection: "column",
+        ...style
+      }}
+    >
       <div style={{ flexGrow: 1 }} />
       <div style={{ flexShrink: 0 }}>{children}</div>
       <div style={{ flexGrow: 2 }} />
