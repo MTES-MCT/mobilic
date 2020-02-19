e309e5ccc90409513b5365840902bfd2e8ae66ae
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-02-19 18:46:15 +0100

    Align expenditure checkboxed

--- src/common/components/Expenditures.js
+++ src/common/components/Expenditures.js
@@ -3,6 +3,7 @@ import Typography from "@material-ui/core/Typography";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import { EXPENDITURES } from "../utils/expenditures";
+import Grid from "@material-ui/core/Grid";
 
 export function Expenditures({ expenditures, pushNewExpenditure }) {
   return (
@@ -10,34 +11,47 @@ export function Expenditures({ expenditures, pushNewExpenditure }) {
       <Typography variant="h6" align="left" className="bold">
         Frais
       </Typography>
-      <div className="expenditures-content">
+      <Grid
+        container
+        direction="row"
+        justify="space-between"
+        alignItems="center"
+      >
         {Object.keys(EXPENDITURES).map(expenditureType => (
-          <FormControlLabel
+          <Grid
             key={expenditureType}
-            control={
-              <Checkbox
-                size="small"
-                className="expenditures-checkbox"
-                checked={
-                  expenditures.filter(e => e.type === expenditureType).length >
-                    0 || false
-                }
-                disabled={
-                  expenditures.filter(e => e.type === expenditureType).length >
-                    0 || false
-                }
-                onChange={() => pushNewExpenditure(expenditureType)}
-                color="primary"
-              />
-            }
-            label={
-              <Typography variant="body2">
-                {EXPENDITURES[expenditureType]["label"]}
-              </Typography>
-            }
-          />
+            item
+            xs={6}
+            md={3}
+            justify="flex-start"
+            style={{ textAlign: "left" }}
+          >
+            <FormControlLabel
+              control={
+                <Checkbox
+                  size="small"
+                  className="expenditures-checkbox"
+                  checked={
+                    expenditures.filter(e => e.type === expenditureType)
+                      .length > 0 || false
+                  }
+                  disabled={
+                    expenditures.filter(e => e.type === expenditureType)
+                      .length > 0 || false
+                  }
+                  onChange={() => pushNewExpenditure(expenditureType)}
+                  color="primary"
+                />
+              }
+              label={
+                <Typography variant="body2">
+                  {EXPENDITURES[expenditureType]["label"]}
+                </Typography>
+              }
+            />
+          </Grid>
         ))}
-      </div>
+      </Grid>
     </div>
   );
 }
