24b58de046ac2933aa650f053a9b9173bff2dcea
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-03-30 12:33:28 +0200

    Fix regression in rule alert display

--- src/common/components/RegulationCheck.js
+++ src/common/components/RegulationCheck.js
@@ -24,7 +24,7 @@ export function RegulationCheck({ check }) {
     emoji = "😢";
   }
   return (
-    <Box color={color} py={1} className="flex-column">
+    <Box color={color} py={1} className="flex-row">
       {icon({
         className: "regulation-check-icon",
         color: "inherit",
