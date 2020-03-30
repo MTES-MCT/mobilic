45bc48399079281b64d6bb7800b4c63a1354ceb4
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-03-31 00:57:44 +0200

    Simplify Employee Filter to use material-ui autocomplete component

--- src/admin/components/EmployeeFilter.js
+++ src/admin/components/EmployeeFilter.js
@@ -1,15 +1,9 @@
 import React from "react";
-import FormControl from "@material-ui/core/FormControl";
-import InputLabel from "@material-ui/core/InputLabel";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import Select from "@material-ui/core/Select";
-import Box from "@material-ui/core/Box";
-import Chip from "@material-ui/core/Chip";
-import MenuItem from "@material-ui/core/MenuItem";
 import { formatPersonName } from "../../common/utils/coworkers";
-import Typography from "@material-ui/core/Typography";
-import { OutlinedInput } from "@material-ui/core";
-import { DataFilter } from "./DataFilter";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import TextField from "@material-ui/core/TextField";
+import Checkbox from "@material-ui/core/Checkbox";
 
 const useStyles = makeStyles(theme => ({
   formControl: {
@@ -28,24 +22,11 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 300
-    }
-  }
-};
-
 export function EmployeeFilter({ users, setUsers }) {
   const classes = useStyles();
 
-  const [open, setOpen] = React.useState(false);
-
-  const handleChange = event => {
-    const selectedIds = event.target.value.map(u => u.id);
+  const handleChange = (event, value) => {
+    const selectedIds = value.map(u => u.id);
     setUsers(
       users.map(u => ({
         ...u,
@@ -54,72 +35,34 @@ export function EmployeeFilter({ users, setUsers }) {
     );
   };
 
-  const reset = () => {
-    setUsers(users.map(u => ({ ...u, selected: false })));
-  };
-
   const selectedUsers = users.filter(user => user.selected);
-  const isActive = selectedUsers.length > 0;
   return (
-    <DataFilter reset={reset} isActive={isActive}>
-      <FormControl variant="outlined" className={classes.formControl}>
-        <InputLabel>Employés</InputLabel>
-        <Select
-          multiple
-          value={selectedUsers}
-          onChange={handleChange}
-          open={open}
-          onClick={e => {
-            setOpen(!open);
-          }}
-          renderValue={people => (
-            <Box className={classes.chips}>
-              {people
-                .filter(p => p.selected)
-                .map(person => (
-                  <Chip
-                    key={person.id}
-                    label={formatPersonName(person)}
-                    className={classes.chip}
-                    onDelete={e => {
-                      e.stopPropagation();
-                      setUsers(
-                        users.map(u =>
-                          u.id === person.id ? { ...u, selected: false } : u
-                        )
-                      );
-                    }}
-                  />
-                ))}
-            </Box>
-          )}
-          input={<OutlinedInput label="Employés" />}
-          MenuProps={MenuProps}
-        >
-          {users.map(user => (
-            <Box key={user.id}>
-              <MenuItem
-                selected={user.selected || false}
-                onClick={e => {
-                  e.stopPropagation();
-                  setUsers(
-                    users.map(u =>
-                      u.id === user.id ? { ...u, selected: !user.selected } : u
-                    )
-                  );
-                }}
-              >
-                <Typography
-                  variant="body1"
-                  className={user.selected ? "bold" : ""}
-                >
-                  {formatPersonName(user)}
-                </Typography>
-              </MenuItem>
-            </Box>
-          ))}
-        </Select>
-      </FormControl>
-    </DataFilter>
+    <Autocomplete
+      multiple
+      id="employee-filter"
+      options={users}
+      disableCloseOnSelect
+      getOptionLabel={option => formatPersonName(option)}
+      renderOption={option => (
+        <>
+          <Checkbox
+            style={{ marginRight: 8 }}
+            checked={option.selected || false}
+          />
+          {formatPersonName(option)}
+        </>
+      )}
+      value={selectedUsers}
+      onChange={handleChange}
+      renderInput={params => (
+        <TextField
+          className={classes.formControl}
+          {...params}
+          variant="outlined"
+          label="Employés"
+          placeholder="Filtrer les employés"
+        />
+      )}
+    />
   );
 }
