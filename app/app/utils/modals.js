83bebbc2ebe0585fef72a62e737fd17bdb2b45a0
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-04-29 10:38:54 +0200

    Remove mobile app

--- app/app/utils/modals.js
+++
@@ -1,5 +0,0 @@
-import {ConfirmationModal} from '../components/Confirmation';
-
-export const MODAL_DICT = {
-  confirmation: ConfirmationModal,
-};
--- app/index.ios.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-import Root from './app/root';
-
-AppRegistry.registerComponent(appName, () => Root);
