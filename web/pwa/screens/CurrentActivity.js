04eacf03ed4e203e10a734cd41df677614525176
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-05-06 15:26:04 +0200

    Revamped main screen

--- web/pwa/screens/CurrentActivity.js
+++ web/pwa/screens/CurrentActivity.js
@@ -1,48 +1,47 @@
 import React from "react";
-import { TimeLine } from "../components/Timeline";
-import { ActivitySwitchGrid } from "../components/ActivitySwitch";
-import Container from "@material-ui/core/Container";
-import { computeTotalActivityDurations } from "common/utils/metrics";
-import Divider from "@material-ui/core/Divider";
+import { ActivitySwitch } from "../components/ActivitySwitch";
 import { useStoreSyncedWithLocalStorage } from "common/utils/store";
 import { resolveTeam } from "common/utils/coworkers";
-import {ACTIVITIES} from "common/utils/activities";
+import { CurrentActivityOverview } from "../components/CurrentActivityOverview";
+import { ActivityList } from "../components/ActivityList";
+import { getTime } from "common/utils/events";
 
 export function CurrentActivity({
   currentActivity,
+  currentMission,
   currentDayActivityEvents,
+  currentMissionActivities,
   pushNewActivityEvent,
   editActivityEvent,
   endMission
 }) {
   const store = useStoreSyncedWithLocalStorage();
 
-  const timers = computeTotalActivityDurations(
-    currentDayActivityEvents,
-    Date.now() + 1
-  );
-
-  return (
-    <Container
-      className="activity-switch-container full-height space-between"
-      maxWidth={false}
-    >
-      <TimeLine
-        dayActivityEvents={currentDayActivityEvents}
-        editActivityEvent={editActivityEvent}
-        pushNewActivityEvent={pushNewActivityEvent}
-      />
-      <Divider className="full-width-divider" />
-      <ActivitySwitchGrid
-        timers={timers}
-        team={resolveTeam(store)}
-        currentActivity={currentActivity}
-        pushActivitySwitchEvent={(activityType, driver = null) =>
-          activityType === ACTIVITIES.rest.name
-            ? endMission()
-            : pushNewActivityEvent({ activityType, driver })
-        }
-      />
-    </Container>
-  );
+  return [
+    <CurrentActivityOverview
+      key={0}
+      currentDayStart={getTime(currentDayActivityEvents[0])}
+      currentActivity={currentActivity}
+    />,
+    <ActivitySwitch
+      key={1}
+      team={resolveTeam(store)}
+      currentActivity={currentActivity}
+      pushActivitySwitchEvent={(activityType, driver = null) =>
+        pushNewActivityEvent({
+          activityType,
+          driver,
+          missionId: currentMission.id
+        })
+      }
+      endMission={endMission}
+    />,
+    <ActivityList
+      key={2}
+      mission={currentMission}
+      activities={currentMissionActivities}
+      editActivityEvent={editActivityEvent}
+      previousMissionEnd={0}
+    />
+  ];
 }
