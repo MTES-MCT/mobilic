821a100aaa89859e359c1a100f8554bbf30d3e70
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-05-11 10:54:55 +0200

    Add spinning loader on main cta button

--- web/pwa/components/MainCtaButton.js
+++ web/pwa/components/MainCtaButton.js
@@ -1,26 +1,47 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import CircularProgress from "@material-ui/core/CircularProgress/CircularProgress";
 
-const useStyles = makeStyles(theme =>({
+const useStyles = makeStyles(theme => ({
   cta: {
     backgroundColor: theme.palette.primary.dark,
-    '&:hover': {
+    "&:hover": {
       backgroundColor: theme.palette.primary.main
     }
   }
 }));
 
-export function MainCtaButton (props) {
+export function MainCtaButton(props) {
   const classes = useStyles();
+  const [loading, setLoading] = React.useState(false);
   return (
     <Button
       className={classes.cta}
       variant="contained"
       color="primary"
+      onClick={async () => {
+        setLoading(true);
+        try {
+          await props.onClick();
+          setLoading(false);
+        } catch (err) {
+          setLoading(false);
+          throw err;
+        }
+      }}
       {...props}
     >
-      {props.children}
+      <span style={{ position: "relative", visibility: loading && "hidden" }}>
+        {props.children}
+      </span>
+      {loading && (
+        <CircularProgress
+          style={{ position: "absolute" }}
+          color="inherit"
+          size="1rem"
+        />
+      )}
     </Button>
-  )
+  );
 }
