c4a08dcd96d3d17c69d5fdf48c115ffd07caa1ad
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-05-20 14:12:04 +0200

    Add home image

--- web/pwa/screens/BeforeWork.js
+++ web/pwa/screens/BeforeWork.js
@@ -2,11 +2,33 @@ import React from "react";
 import Container from "@material-ui/core/Container";
 import { PlaceHolder } from "../../common/PlaceHolder";
 import Typography from "@material-ui/core/Typography";
+import { ReactComponent as HomeIcon } from "common/assets/images/Home.svg";
 import { useModals } from "common/utils/modals";
 import Box from "@material-ui/core/Box";
 import { AccountButton } from "../components/AccountButton";
 import { MainCtaButton } from "../components/MainCtaButton";
 import Button from "@material-ui/core/Button";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    position: "relative",
+    justifyContent: "flex-start",
+    flexGrow: 1
+  },
+  heroImage: {
+    width: "100%",
+    height: "auto",
+    color: theme.palette.background.default
+  },
+  accountButton: {
+    alignSelf: "flex-end",
+    position: "absolute",
+    top: "0",
+    right: "0"
+  }
+}));
 
 export function BeforeWork({ beginNewMission, missions }) {
   const modals = useModals();
@@ -54,14 +76,21 @@ export function BeforeWork({ beginNewMission, missions }) {
     });
   };
 
+  const classes = useStyles();
+
   return [
     <Container
       key={1}
-      className="container scrollable"
+      className={`container scrollable ${classes.container}`}
+      disableGutters
       maxWidth={false}
-      style={{ justifyContent: "flex-start", flexGrow: 1 }}
     >
-      <AccountButton py={2} style={{ alignSelf: "flex-end" }} />
+      <AccountButton p={2} className={classes.accountButton} />
+      <SvgIcon
+        viewBox="0 0 506 382"
+        className={classes.heroImage}
+        component={HomeIcon}
+      />
       <PlaceHolder>
         <Typography variant="h3">👋</Typography>
         <Typography variant="h3">Bienvenue sur MobiLIC !</Typography>
