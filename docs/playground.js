3a7d576886a3af0c181aefea89c42f82a1ae5915
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-07-06 11:03:16 +0200

    Add header to playground

--- docs/playground.js
+++ docs/playground.js
@@ -1,14 +1,76 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import map from "lodash/map";
 import "./playground.css";
 import { Provider } from "react-redux";
 import { Playground, store } from "graphql-playground-react";
 
-const API_HOST = process.env.REACT_APP_API_HOST || "/api";
+const ENVS = {
+  prod: {
+    host: "https://api.mobilic.beta.gouv.fr",
+    color: "#ff9947",
+    label: "PRODUCTION"
+  },
+  sandbox: {
+    host: "https://mobilic-api-sandbox.herokuapp.com",
+    color: "#03bd5b",
+    label: "BAC A SABLE"
+  },
+  staging: {
+    host: "https://mobilic-api-staging.herokuapp.com",
+    color: "#ff9947",
+    label: "RECETTE"
+  }
+};
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Playground endpoint={API_HOST + "/graphql"} />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Playgrounds />, document.getElementById("root"));
+
+function Playgrounds() {
+  const [env, setEnv] = React.useState("sandbox");
+  return [
+    <div
+      key={0}
+      style={{
+        display: "flex",
+        height: "50",
+        backgroundColor: ENVS[env].color,
+        paddingRight: "8px",
+        paddingLeft: "8px",
+        justifyContent: "space-between"
+      }}
+    >
+      <p
+        style={{
+          textTransform: "uppercase",
+          color: "white",
+          fontWeight: "bold"
+        }}
+      >
+        {env === "prod" && "⚠️ "}environnement : {ENVS[env].label}
+      </p>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        {map(ENVS, (value, entry) => (
+          <button
+            style={{
+              cursor: "pointer",
+              paddingTop: "6px",
+              paddingBottom: "6px",
+              borderStyle: "solid",
+              borderRadius: 4,
+              color: env === entry && "white",
+              backgroundColor: env === entry && "#003b80"
+            }}
+            key={entry}
+            onClick={() => setEnv(entry)}
+            disabled={env === entry}
+          >
+            {value.label}
+          </button>
+        ))}
+      </div>
+    </div>,
+    <Provider key={1} store={store}>
+      <Playground endpoint={ENVS[env].host + "/graphql"} />
+    </Provider>
+  ];
+}
