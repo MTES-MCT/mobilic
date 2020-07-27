87bb9869fd45089e8d3839de81b659ca26d74d9b
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-07-28 00:40:06 +0200

    Refactor laoding button to be used globally

---
+++ common/components/LoadingButton.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress/CircularProgress";
+
+export function LoadingButton(props) {
+  const [loading, setLoading] = React.useState(false);
+
+  async function onClick(e) {
+    if (props.loading === undefined) {
+      await props.onClick(e);
+      return;
+    }
+    setLoading(true);
+    try {
+      await props.onClick(e);
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      throw err;
+    }
+  }
+
+  const isLoading = props.loading !== undefined ? props.loading : loading;
+
+  return (
+    <Button
+      onClick={props.onClick ? onClick : null}
+      {...props}
+      disabled={props.disabled || isLoading}
+    >
+      <span style={{ position: "relative", visibility: isLoading && "hidden" }}>
+        {props.children}
+      </span>
+      {isLoading && (
+        <CircularProgress
+          style={{ position: "absolute" }}
+          color="inherit"
+          size="1rem"
+        />
+      )}
+    </Button>
+  );
+}
--- web/pwa/components/MainCtaButton.js
+++ web/pwa/components/MainCtaButton.js
@@ -1,7 +1,6 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import CircularProgress from "@material-ui/core/CircularProgress/CircularProgress";
+import { LoadingButton } from "common/components/LoadingButton";
 
 const useStyles = makeStyles(theme => ({
   cta: {
@@ -14,34 +13,12 @@ const useStyles = makeStyles(theme => ({
 
 export function MainCtaButton(props) {
   const classes = useStyles();
-  const [loading, setLoading] = React.useState(false);
   return (
-    <Button
+    <LoadingButton
       className={classes.cta}
       variant="contained"
       color="primary"
-      onClick={async () => {
-        setLoading(true);
-        try {
-          await props.onClick();
-          setLoading(false);
-        } catch (err) {
-          setLoading(false);
-          throw err;
-        }
-      }}
       {...props}
-    >
-      <span style={{ position: "relative", visibility: loading && "hidden" }}>
-        {props.children}
-      </span>
-      {loading && (
-        <CircularProgress
-          style={{ position: "absolute" }}
-          color="inherit"
-          size="1rem"
-        />
-      )}
-    </Button>
+    />
   );
 }
