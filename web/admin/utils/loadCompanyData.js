59e3a7cd5c6223994b872c13b7250a6fe9b968a6
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-09-23 10:58:53 +0200

    Revert "Temporarily alter history depth"
    This reverts commit c5faf700ca5c7b72f081f587b84eef9f75744f42.

--- web/admin/utils/loadCompanyData.js
+++ web/admin/utils/loadCompanyData.js
@@ -1,10 +1,10 @@
 import { COMPANY_QUERY } from "common/utils/api";
-// import { DAY } from "common/utils/time";
+import { DAY } from "common/utils/time";
 
 export async function loadCompanyData(api, companyId) {
   const companyResponse = await api.graphQlQuery(COMPANY_QUERY, {
     id: companyId,
-    activityAfter: "2019-07-01"
+    activityAfter: new Date(Date.now() - DAY * 180).toISOString().slice(0, 10)
   });
   const company = companyResponse.data.company;
   return {
