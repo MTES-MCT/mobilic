bbd58f8774be9884a174c0a6147ce96ee098a614
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-10-04 23:34:00 +0200

    Set max queue size on non concurrent queues

--- common/utils/concurrency.js
+++ common/utils/concurrency.js
@@ -1,37 +1,55 @@
 export class NonConcurrentExecutionQueue {
-  constructor() {
+  constructor(maxQueueSize) {
     this.queue = [];
-    this.lock = false;
+    this.actualQueueSize = 0;
+    this.maxQueueSize = maxQueueSize;
   }
 
   execute = func => {
     return new Promise((resolve, reject) => {
-      const executeOperation = async () => {
-        this.lock = true;
+      const executeOperation = async (
+        func,
+        decrementActualQueueSize = true
+      ) => {
         let response, error;
         try {
           response = await func();
         } catch (err) {
           error = err;
         }
-        if (this.queue.length === 0) {
-          this.lock = false;
-        } else {
+
+        // Take out the task from the queue
+        if (decrementActualQueueSize && this.actualQueueSize > 0) {
+          this.actualQueueSize = this.actualQueueSize - 1;
+        }
+        this.queue = this.queue.slice(1);
+
+        // Start the next task if it exists
+        if (this.queue.length > 0) {
           this.queue[0]();
-          this.queue = this.queue.slice(1);
         }
         return error ? reject(error) : resolve(response);
       };
-      if (!this.lock) {
-        executeOperation();
+
+      // Enqueue the job
+      if (!this.maxQueueSize || this.actualQueueSize < this.maxQueueSize) {
+        this.actualQueueSize = this.actualQueueSize + 1;
+        this.queue.push(async () => await executeOperation(func));
       } else {
-        this.queue.push(executeOperation);
+        this.queue.push(async () => await executeOperation(resolve, false));
+      }
+
+      // Immediately start it if the queue was empty;
+      if (this.queue.length === 1) {
+        this.queue[0]();
       }
     });
   };
 
   clear = () => {
-    this.queue = [];
-    this.lock = false;
+    if (this.queue.length > 0) {
+      this.queue = this.queue.slice(0, 1);
+    } else this.queue = [];
+    this.actualQueueSize = 0;
   };
 }
