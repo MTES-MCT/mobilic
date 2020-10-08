6e42e9230bde2a4674407894d0b29a80439664f6
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-10-08 13:04:06 +0200

    Hide marianne on little screens

--- web/common/Logos.js
+++ web/common/Logos.js
@@ -5,19 +5,23 @@ import SvgIcon from "@material-ui/core/SvgIcon";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import { LinkButton } from "./LinkButton";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 export function Logos() {
+  const shouldDisplayBothLogos = useMediaQuery("(min-width:350px)");
   return (
     <LinkButton style={{ borderRadius: 0, textTransform: "none" }} href="/">
       <Box className="flex-row-flex-start">
-        <Box mr={2}>
-          <SvgIcon
-            style={{ fontSize: 100 }}
-            viewBox="40 40 210 200"
-            className="marianne-logo"
-            component={MarianneIcon}
-          />
-        </Box>
+        {shouldDisplayBothLogos && (
+          <Box mr={2}>
+            <SvgIcon
+              style={{ fontSize: 100 }}
+              viewBox="40 40 210 200"
+              className="marianne-logo"
+              component={MarianneIcon}
+            />
+          </Box>
+        )}
         <Typography className="project-name">mobilic</Typography>
         <SvgIcon
           viewBox="0 0 117 40"
