ee2df593dc2a857d41766a07c79398d494093b31
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-12-04 23:05:58 +0100

    update package.json

--- web/admin/utils/loadCompaniesData.js
+++ web/admin/utils/loadCompaniesData.js
@@ -4,10 +4,10 @@ import { DAY } from "common/utils/time";
 export async function loadCompaniesData(api, userId) {
   const companyResponse = await api.graphQlQuery(ADMIN_COMPANIES_QUERY, {
     id: userId,
-    activityAfter: new Date(Date.now() - DAY * 1000 * 120)
+    activityAfter: new Date(Date.now() - DAY * 1000 * 150)
       .toISOString()
       .slice(0, 10),
-    workDaysLimit: 250
+    workDaysLimit: 10000
   });
   return companyResponse.data.user.adminedCompanies;
 }
--- web/admin/utils/navigation.js
+++ web/admin/utils/navigation.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { ActivityPanel } from "../panels/Activities";
 import { CompanyPanel } from "../panels/Company";
+import { ValidationPanel } from "../panels/Validations";
 
 export const ADMIN_VIEWS = [
   {
     label: "Entreprise",
     route: "/company",
-    component: <CompanyPanel />,
+    component: props => <CompanyPanel {...props} />,
     isDefault: true
   },
   {
     label: "Activités",
     route: "/activities",
-    component: <ActivityPanel />
+    component: props => <ActivityPanel {...props} />
+  },
+  {
+    label: "Saisies à valider",
+    route: "/validations",
+    component: props => <ValidationPanel {...props} />
   }
 ];
