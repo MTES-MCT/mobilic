dfa36c25d7656a8d5873af5a3bd8bf213f5982c8
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2021-04-11 20:57:48 +0200

    More explicit chunk names

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -268,8 +268,7 @@ module.exports = function(webpackEnv) {
       // https://twitter.com/wSokra/status/969633336732905474
       // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
       splitChunks: {
-        chunks: "all",
-        name: false
+        chunks: "all"
       },
       // Keep the runtime chunk separated to enable long term caching
       // https://twitter.com/wSokra/status/969679223278505985
