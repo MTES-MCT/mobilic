0b71747d454d27e3ef24ced012d7a04b298533fa
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2021-11-10 15:03:46 +0100

    Ability to resend activation email.

--- common/utils/apiQueries.js
+++ common/utils/apiQueries.js
@@ -569,6 +569,15 @@ export const CHANGE_EMAIL_MUTATION = gql`
     }
   }
 `;
+export const RESEND_ACTIVATION_EMAIL = gql`
+  mutation resendActivationEmail($email: String!) {
+    account {
+      resendActivationEmail(email: $email) {
+        success
+      }
+    }
+  }
+`;
 export const RESET_PASSWORD_MUTATION = gql`
   mutation resetPassword($token: String!, $password: String!) {
     account {
--- web/home/AccountInfo.js
+++ web/home/AccountInfo.js
@@ -17,9 +17,18 @@ import Divider from "@material-ui/core/Divider";
 import AlertTitle from "@material-ui/lab/AlertTitle";
 import { PaperContainer, PaperContainerTitle } from "../common/PaperContainer";
 import { frenchFormatDateStringOrTimeStamp } from "common/utils/time";
-import { CHANGE_EMAIL_MUTATION } from "common/utils/apiQueries";
+import {
+  CHANGE_EMAIL_MUTATION,
+  RESEND_ACTIVATION_EMAIL
+} from "common/utils/apiQueries";
 import { EmploymentInfoCard } from "../common/EmploymentInfoCard";
 import { employmentSelector } from "common/store/selectors";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { useSnackbarAlerts } from "../common/Snackbar";
+import { formatApiError } from "common/utils/errors";
+import List from "@material-ui/core/List";
+import { ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   innerContainer: {
@@ -27,6 +36,19 @@ const useStyles = makeStyles(theme => ({
   },
   inactiveAlert: {
     marginTop: theme.spacing(2)
+  },
+  activationIntroLabel: {
+    paddingBottom: theme.spacing(2)
+  },
+  actionButton: {
+    marginTop: theme.spacing(2)
+  },
+  emailCheckIcon: {
+    color: "rgb(85, 21, 15)",
+    minWidth: theme.spacing(5)
+  },
+  emailCheckItem: {
+    alignItems: "baseline"
   }
 }));
 
@@ -49,11 +71,24 @@ export default function Home() {
   const store = useStoreSyncedWithLocalStorage();
   const api = useApi();
   const employments = employmentSelector(store.state);
-
+  const alerts = useSnackbarAlerts();
   const modals = useModals();
 
   const userInfo = store.userInfo();
   const isActive = userInfo.hasActivatedEmail;
+
+  async function resendActivationEmail() {
+    try {
+      await api.graphQlMutate(
+        RESEND_ACTIVATION_EMAIL,
+        { email: userInfo.email },
+        { context: { nonPublicApi: true } }
+      );
+      alerts.success("Le mail a été renvoyé avec succès", userInfo.email, 6000);
+    } catch (err) {
+      alerts.error(formatApiError(err), userInfo.email, 6000);
+    }
+  }
 
   return [
     <Header key={0} />,
@@ -108,13 +143,76 @@ export default function Home() {
                 }
                 alertComponent={
                   isActive ? null : (
-                    <Alert severity="error" className={classes.inactiveAlert}>
+                    <Alert
+                      key={0}
+                      severity="error"
+                      className={classes.inactiveAlert}
+                    >
                       <AlertTitle className="bold">
                         Adresse email non activée
                       </AlertTitle>
-                      Un email d'activation vous a été envoyé à l'adresse
-                      ci-dessus. L'activation est nécessaire pour accéder aux
-                      services Mobilic.
+                      <Typography className={classes.activationIntroLabel}>
+                        Un email d'activation vous a été envoyé à l'adresse
+                        ci-dessus. L'activation est nécessaire pour accéder aux
+                        services Mobilic.
+                      </Typography>
+                      <List disablePadding>
+                        <ListItem
+                          disableGutters
+                          dense
+                          className={classes.emailCheckItem}
+                        >
+                          <ListItemIcon className={classes.emailCheckIcon}>
+                            👉
+                          </ListItemIcon>
+                          <ListItemText primary="Il peut mettre quelques minutes à arriver, merci pour votre patience" />
+                        </ListItem>
+                        <ListItem
+                          disableGutters
+                          dense
+                          className={classes.emailCheckItem}
+                        >
+                          <ListItemIcon className={classes.emailCheckIcon}>
+                            👉
+                          </ListItemIcon>
+                          <ListItemText primary="Merci de vérifier vos spams" />
+                        </ListItem>
+                        <ListItem
+                          disableGutters
+                          dense
+                          className={classes.emailCheckItem}
+                        >
+                          <ListItemIcon className={classes.emailCheckIcon}>
+                            👉
+                          </ListItemIcon>
+                          <ListItemText primary="Pour recevoir nos emails sans encombres, vous pouvez ajouter mobilic@beta.gouv.fr à votre liste de contacts" />
+                        </ListItem>
+                        <ListItem
+                          disableGutters
+                          dense
+                          className={classes.emailCheckItem}
+                        >
+                          <ListItemIcon className={classes.emailCheckIcon}>
+                            👉
+                          </ListItemIcon>
+                          <ListItemText primary="Si vous n'avez pas reçu l'email de confirmation au bout d'une demi-heure, vous pouvez le renvoyer en cliquant sur le bouton ci dessous :" />
+                        </ListItem>
+                        <ListItem
+                          disableGutters
+                          dense
+                          className={classes.emailCheckItem}
+                        >
+                          <ListItemIcon className={classes.emailCheckIcon} />
+                          <Button
+                            size="small"
+                            color="primary"
+                            variant="contained"
+                            onClick={resendActivationEmail}
+                          >
+                            Renvoyer l'email d'activation
+                          </Button>
+                        </ListItem>
+                      </List>
                     </Alert>
                   )
                 }
