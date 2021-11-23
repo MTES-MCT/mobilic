b5e644abdf8c89ede9c9b4bf3573a17855de5e70
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2021-11-23 15:16:13 +0100

    First team feedbacks

--- web/landing/ResourcePage/FaqCard.js
+++ web/landing/ResourcePage/FaqCard.js
@@ -5,25 +5,41 @@ import { Card } from "@material-ui/core";
 import Divider from "@material-ui/core/Divider";
 import { ChevronRight } from "@material-ui/icons";
 import { Link, LinkButton } from "../../common/LinkButton";
+import Box from "@material-ui/core/Box";
 
 const useStyles = makeStyles(theme => ({
   faqCard: {
     borderRadius: 10,
     padding: theme.spacing(2),
-    backgroundColor: theme.palette.grey[100]
+    backgroundColor: theme.palette.grey[100],
+    borderColor: theme.palette.primary.main,
+    height: "100%"
   },
   faqCardAnswer: {
     marginTop: theme.spacing(1),
     marginBottom: theme.spacing(2)
   },
   faqCardDivider: {
-    marginBottom: theme.spacing(2)
+    marginBottom: theme.spacing(1)
   },
   linkWholeCard: {
-    textTransform: "none"
+    textTransform: "none",
+    height: "100%",
+    "& .MuiButton-label": {
+      height: "100%"
+    }
   },
   faqCardButtonIcon: {
     verticalAlign: "middle"
+  },
+  moreInfoBlock: {
+    position: "absolute",
+    bottom: theme.spacing(2),
+    left: theme.spacing(3),
+    right: theme.spacing(3)
+  },
+  dummy: {
+    height: theme.spacing(6)
   }
 }));
 
@@ -31,15 +47,18 @@ export function FaqCard({ question, answer, link }) {
   const classes = useStyles();
 
   return (
-    <LinkButton href={link} className={classes.linkWholeCard}>
+    <LinkButton href={link} className={classes.linkWholeCard} color="primary">
       <Card variant="outlined" className={classes.faqCard}>
         <Typography variant={"h4"}>{question}</Typography>
         <Typography className={classes.faqCardAnswer}>{answer}</Typography>
-        <Divider className={classes.faqCardDivider} />
-        <Link href={link}>
-          Plus d'infos
-          <ChevronRight className={classes.faqCardButtonIcon} />
-        </Link>
+        <Box className={classes.dummy}></Box>
+        <Box className={classes.moreInfoBlock}>
+          <Divider className={classes.faqCardDivider} />
+          <Link href={link}>
+            Plus d'infos
+            <ChevronRight className={classes.faqCardButtonIcon} />
+          </Link>
+        </Box>
       </Card>
     </LinkButton>
   );
