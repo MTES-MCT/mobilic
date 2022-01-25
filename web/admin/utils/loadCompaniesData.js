ca3f0dc86472ce297bd889eba3e5248ba02634d8
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2022-01-25 18:37:04 +0100

    Increase timeout when loading company data

--- web/admin/utils/loadCompaniesData.js
+++ web/admin/utils/loadCompaniesData.js
@@ -2,11 +2,15 @@ import { DAY, getStartOfDay, now } from "common/utils/time";
 import { ADMIN_COMPANIES_QUERY } from "common/utils/apiQueries";
 
 export async function loadCompaniesData(api, userId, minDate) {
-  const companyResponse = await api.graphQlQuery(ADMIN_COMPANIES_QUERY, {
-    id: userId,
-    activityAfter: minDate,
-    workDaysLimit: 10000,
-    endedMissionsAfter: getStartOfDay(now() - DAY * 31)
-  });
+  const companyResponse = await api.graphQlQuery(
+    ADMIN_COMPANIES_QUERY,
+    {
+      id: userId,
+      activityAfter: minDate,
+      workDaysLimit: 10000,
+      endedMissionsAfter: getStartOfDay(now() - DAY * 31)
+    },
+    { context: { timeout: 30000 } }
+  );
   return companyResponse.data.user.adminedCompanies;
 }
