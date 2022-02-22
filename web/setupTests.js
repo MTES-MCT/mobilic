a9948fcae3e37dd1d237f47da97611c437f41a89
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2022-02-22 20:00:00 +0100

    chore: upgrade enryme adapter react

--- web/setupTests.js
+++ web/setupTests.js
@@ -4,7 +4,7 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
 
-const Enzyme = require("enzyme");
-const Adapter = require("enzyme-adapter-react-16");
+import Enzyme from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 Enzyme.configure({ adapter: new Adapter() });
