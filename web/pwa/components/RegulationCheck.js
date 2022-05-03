ea425d759a07e7b72c819aa4ec9cfa570ae6fe88
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2022-05-03 12:37:29 +0200

    feat(regulation check): use material alerts

--- web/pwa/components/RegulationCheck.js
+++ web/pwa/components/RegulationCheck.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { RULE_RESPECT_STATUS } from "common/utils/regulation/rules";
-import Box from "@mui/material/Box";
 import { ALERT_TYPE_PROPS } from "common/utils/regulation/alertTypes";
+import { Alert } from "@mui/material";
 import { Link } from "../../common/LinkButton";
 import { useRegulationDrawer } from "../../landing/ResourcePage/RegulationDrawer";
 import { ChevronRight } from "@mui/icons-material";
@@ -10,8 +10,7 @@ import { makeStyles } from "@mui/styles";
 const useStyles = makeStyles(theme => ({
   moreInfoIcon: {
     verticalAlign: "middle",
-    marginBottom: theme.spacing(0.25),
-    color: theme.palette.primary.main
+    marginBottom: theme.spacing(0.25)
   }
 }));
 
@@ -19,20 +18,16 @@ export function RegulationCheck({ check }) {
   const openRegulationDrawer = useRegulationDrawer();
 
   const classes = useStyles();
-  let color;
-  let emoji;
 
   if (!check) return null;
 
+  let severity;
   if (check.status === RULE_RESPECT_STATUS.success) {
-    color = "success.main";
-    emoji = "👏";
+    severity = "success";
   } else if (check.status === RULE_RESPECT_STATUS.pending) {
-    color = "warning.main";
-    emoji = "🙂";
+    severity = "warning";
   } else {
-    color = "error.main";
-    emoji = "😢";
+    severity = "error";
   }
 
   const alertProps = ALERT_TYPE_PROPS[check.rule];
@@ -40,21 +35,17 @@ export function RegulationCheck({ check }) {
   const message = alertProps.format(check.status, check.extra || {});
 
   return (
-    <Box color={color} py={1} className="flex-row">
-      <span>{` ${emoji}`}</span>
-      <Box pl={1}>
-        <Link
-          variant="body2"
-          color="inherit"
-          onClick={e => {
-            e.preventDefault();
-            openRegulationDrawer(rule, true);
-          }}
-        >
-          {message}
-          <ChevronRight className={classes.moreInfoIcon} />
-        </Link>
-      </Box>
-    </Box>
+    <Alert severity={severity}>
+      <Link
+        color="inherit"
+        onClick={e => {
+          e.preventDefault();
+          openRegulationDrawer(rule, true);
+        }}
+      >
+        {message}
+        <ChevronRight className={classes.moreInfoIcon} />
+      </Link>
+    </Alert>
   );
 }
