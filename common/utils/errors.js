b8647dff4b8c432597cb76130b80eecdc5ca7d20
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2022-08-29 15:13:05 +0200

    feat: update error code

--- common/utils/errors.js
+++ common/utils/errors.js
@@ -193,9 +193,9 @@ export function defaultFormatGraphQLApiError(graphQLError, store) {
         return "L'action de mettre fin à votre rattachement ne peut être effectuée que par un autre gestionnaire.";
       case "INVALID_CONTROL_TOKEN":
         return "Le QR Code n'a pas été reconnu par Mobilic.";
-      case "ACTIVITY_BEFORE_EMPLOYMENT_EMPLOYEE":
+      case "ACTIVITY_OUTSIDE_EMPLOYMENT_EMPLOYEE":
         return "Vous ne pouvez pas enregistrer des activités en dehors de votre période de rattachement.";
-      case "ACTIVITY_BEFORE_EMPLOYMENT_ADMIN":
+      case "ACTIVITY_OUTSIDE_EMPLOYMENT_ADMIN":
         return "Vous ne pouvez pas enregistrer des activités en dehors de la période de rattachement du salarié sélectionné.";
       default:
         return null;
