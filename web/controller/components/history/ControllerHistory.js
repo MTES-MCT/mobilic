eadec530f87049d08300de86c49080f746f68912
Tristan Gueguen <tristan.gueguen@gmail.com>
2022-09-12 10:37:25 +0200

    fixed import

--- web/controller/components/history/ControllerHistory.js
+++ web/controller/components/history/ControllerHistory.js
@@ -4,7 +4,10 @@ import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import { Modal, ModalTitle, ModalContent } from "@dataesr/react-dsfr";
 import { Header } from "../../../common/Header";
-import { ControllerHistoryFilters, CONTROL_TYPES } from "./Filters";
+import {
+  ControllerHistoryFilters,
+  CONTROL_TYPES
+} from "./ControllerHistoryFilters";
 import { useLoadControls } from "../../utils/loadControls";
 import { useStoreSyncedWithLocalStorage } from "common/store/store";
 import { ControlsList } from "../list/ControlsList";
