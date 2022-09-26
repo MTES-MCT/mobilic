c16bf0d7aa01bc4972367c15e5cd95d3b8a99b96
Tristan Gueguen <tristan.gueguen@gmail.com>
2022-09-26 13:06:49 +0200

    material icon for gavel - good links

--- web/controller/components/help/ModalHelpController.js
+++ web/controller/components/help/ModalHelpController.js
@@ -1,33 +1,64 @@
 import React from "react";
 import { ControllerHelpCard } from "../home/ControllerHelpCard";
 import Stack from "@mui/material/Stack";
+import GavelIcon from "@mui/icons-material/Gavel";
+import SvgIcon from "@mui/material/SvgIcon";
+import { makeStyles } from "@mui/styles";
+import classNames from "classnames";
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    color: theme.palette.primary.main
+  }
+}));
+
+const getDSFRIconComponent = (iconName, classes) => (
+  <span
+    className={classNames(`${iconName} fr-icon--lg`, classes.icon)}
+    aria-hidden="true"
+  ></span>
+);
 
 export function HelpController() {
+  const classes = useStyles();
+
   return (
     <Stack spacing={2}>
       <ControllerHelpCard
-        icon="fr-icon-lightbulb-fill fr-icon--lg"
+        iconComponent={
+          <SvgIcon
+            viewBox="0 0 20 20"
+            className={classes.icon}
+            component={GavelIcon}
+          />
+        }
         title="NATINF expliqués"
         description="Comprendre les codes associés aux seuils réglementaires"
         linkTo="https://app.gitbook.com/invite/-MMKfijmgB1Sviz-n2vh/DgrUI15YCohwADMt91bF"
       />
       <ControllerHelpCard
-        icon="fr-icon-question-fill fr-icon--lg"
+        iconComponent={getDSFRIconComponent("fr-icon-question-fill", classes)}
         title="FAQ"
         description="Trouvez les réponses à vos questions"
         linkTo="https://app.gitbook.com/o/-MMKfijmgB1Sviz-n2vh/s/5P00lZMB3lX6eHmhKqiJ/"
       />
       <ControllerHelpCard
-        icon="fr-icon-git-repository-fill fr-icon--lg"
+        iconComponent={getDSFRIconComponent(
+          "fr-icon-git-repository-fill",
+          classes
+        )}
         title="Documentation"
         description="Comment utiliser Mobilic ?"
-        linkTo="https://app.gitbook.com/o/-MMKfijmgB1Sviz-n2vh/s/5P00lZMB3lX6eHmhKqiJ/"
+        linkTo="https://mobilic.beta.gouv.fr/resources/controller"
       />
       <ControllerHelpCard
-        icon="fr-icon-question-answer-line fr-icon--lg"
+        iconComponent={getDSFRIconComponent(
+          "fr-icon-question-answer-line",
+          classes
+        )}
         title="Messagerie instantanée"
         description="Échangez avec l’équipe Mobilic et d’autres CTT sur notre forum Tchap !"
-        linkTo="https://app.gitbook.com/o/-MMKfijmgB1Sviz-n2vh/s/5P00lZMB3lX6eHmhKqiJ/"
+        linkTo="https://www.tchap.gouv.fr/#/room/%23SupportMobilicYNhe5wcTWWb:agent.dinum.tchap.gouv.fr"
       />
     </Stack>
   );
--- web/controller/components/home/ControllerHelpCard.js
+++ web/controller/components/home/ControllerHelpCard.js
@@ -4,24 +4,23 @@ import { makeStyles } from "@mui/styles";
 import classNames from "classnames";
 
 const useStyles = makeStyles(theme => ({
-  icon: {
-    color: theme.palette.primary.main
-  },
   card: {
     padding: theme.spacing(4)
   }
 }));
 
-export function ControllerHelpCard({ icon, title, description, linkTo }) {
+export function ControllerHelpCard({
+  iconComponent,
+  title,
+  description,
+  linkTo
+}) {
   const classes = useStyles();
   return (
     <a href={linkTo} target="_blank" rel="noreferrer">
       <div className={classNames("fr-tile", classes.card)}>
         <Stack direction="row" spacing={4}>
-          <span
-            className={classNames(`${icon}`, classes.icon)}
-            aria-hidden="true"
-          ></span>
+          {iconComponent}
           <Stack>
             <h5>{title}</h5>
             <p>{description}</p>
