fd61d391bcb0ef74fa3d15115500a132841e0474
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2022-10-13 09:00:46 +0200

    Replace playground with graphiql

--- playground/playground.js
+++ playground/playground.js
@@ -1,71 +1,12 @@
+import { createGraphiQLFetcher } from "@graphiql/toolkit";
+import { GraphiQL } from "graphiql";
 import React from "react";
 import ReactDOM from "react-dom";
-import map from "lodash/map";
-import "./playground.css";
-import { Provider } from "react-redux";
-import { Playground, store } from "graphql-playground-react";
 
-const ENVS = {
-  prod: {
-    host: "https://api.mobilic.beta.gouv.fr",
-    color: "#ff9947",
-    label: "PRODUCTION"
-  },
-  sandbox: {
-    host: "https://api.sandbox.mobilic.beta.gouv.fr",
-    color: "#03bd5b",
-    label: "BAC A SABLE"
-  }
-};
+import "graphiql/graphiql.css";
 
-ReactDOM.render(<Playgrounds />, document.getElementById("root"));
+const fetcher = createGraphiQLFetcher({
+  url: "https://api.mobilic.beta.gouv.fr/graphql"
+});
 
-function Playgrounds() {
-  const [env, setEnv] = React.useState("sandbox");
-  return [
-    <div
-      key={0}
-      style={{
-        display: "flex",
-        height: "50",
-        backgroundColor: ENVS[env].color,
-        paddingRight: "8px",
-        paddingLeft: "8px",
-        justifyContent: "space-between"
-      }}
-    >
-      <p
-        style={{
-          textTransform: "uppercase",
-          color: "white",
-          fontWeight: "bold"
-        }}
-      >
-        {env === "prod" && "⚠️ "}environnement : {ENVS[env].label}
-      </p>
-      <div style={{ display: "flex", alignItems: "center" }}>
-        {map(ENVS, (value, entry) => (
-          <button
-            style={{
-              cursor: "pointer",
-              paddingTop: "6px",
-              paddingBottom: "6px",
-              borderStyle: "solid",
-              borderRadius: 4,
-              color: env === entry && "white",
-              backgroundColor: env === entry && "#003b80"
-            }}
-            key={entry}
-            onClick={() => setEnv(entry)}
-            disabled={env === entry}
-          >
-            {value.label}
-          </button>
-        ))}
-      </div>
-    </div>,
-    <Provider key={1} store={store}>
-      <Playground endpoint={ENVS[env].host + "/graphql"} />
-    </Provider>
-  ];
-}
+ReactDOM.render(<GraphiQL query={""} fetcher={fetcher} />, document.body);
