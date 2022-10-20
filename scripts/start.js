24cb261a9bbf58fa56cfb35aeb237b88c243da9f
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2022-10-20 10:23:01 +0200

    Clean old config file

--- scripts/start.js
+++ scripts/start.js
@@ -26,7 +26,7 @@ const {
 const openBrowser = require("react-dev-utils/openBrowser");
 const paths = require("../config/paths");
 const configFactory = require("../config/webpack.config");
-const createDevServerConfig = require("../config/webpackDevServer4.config");
+const createDevServerConfig = require("../config/webpackDevServer.config");
 
 const useYarn = fs.existsSync(paths.yarnLockFile);
 const isInteractive = process.stdout.isTTY;
