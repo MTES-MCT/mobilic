df6004b3d122f43cc917d71f72664b01f53c1c12
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2023-05-12 15:23:46 +0200

    Test woff2

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -355,7 +355,7 @@ module.exports = function(webpackEnv) {
             // smaller than specified limit in bytes as data URLs to avoid requests.
             // A missing `test` is equivalent to a match.
             {
-              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.woff2$/],
               loader: require.resolve("url-loader"),
               options: {
                 limit: imageInlineSizeLimit,
