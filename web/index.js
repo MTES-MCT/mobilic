123cfa75591aee7e283c07bd8165175e755f84c7
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2023-05-12 14:39:40 +0200

    test 6, add even more logs to service worker

--- web/index.js
+++ web/index.js
@@ -4,8 +4,10 @@ import * as serviceWorker from "./serviceWorker";
 import React from "react";
 import Root from "./root";
 import { initSentry } from "common/utils/sentry";
+console.log("A");
 
 initSentry();
+console.log("B");
 
 // Crisp button
 if (process.env.REACT_APP_CRISP_WEBSITE_ID) {
@@ -16,10 +18,13 @@ if (process.env.REACT_APP_CRISP_WEBSITE_ID) {
   script.async = 1;
   document.getElementsByTagName("head")[0].appendChild(script);
 }
+console.log("C");
 
 ReactDOM.render(<Root />, document.getElementById("root"));
+console.log("D");
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
+console.log("E");
