cb6b9e769c9d178cb63754543ca9949272e32c6d
Tristan Gueguen <tristan.gueguen@gmail.com>
2023-06-08 15:12:27 +0200

    correct link + margin bottom

--- web/controller/components/infractions/InfractionsAvailableSoon.js
+++ web/controller/components/infractions/InfractionsAvailableSoon.js
@@ -4,12 +4,12 @@ import Typography from "@mui/material/Typography";
 
 export function InfractionsAvailableSoon() {
   return (
-    <Alert severity="info" sx={{ height: "fit-content" }}>
+    <Alert severity="info" sx={{ height: "fit-content", marginBottom: 2 }}>
       <Typography>
         Vous pourrez bientôt retenir des infractions pour les contrôles. En
         attendant vous pouvez retrouver{" "}
         <a
-          href="https://mobilic.beta.gouv.fr/resources/regulations"
+          href="https://mobilic.gitbook.io/natinf-expliques/"
           target="_blank"
           rel="noreferrer"
         >
