e0ddc776dc42d9bd70495b1b9ae4c1765193a120
Gaspard "Sarp" Lonchampt <71872843+gaspard-lonchampt@users.noreply.github.com>
2023-11-20 14:50:32 +0100

    Update web/signup/RoleSelection.js
    Co-authored-by: Sandrine Ricardo <68006779+sandrica89@users.noreply.github.com>

--- web/signup/RoleSelection.js
+++ web/signup/RoleSelection.js
@@ -24,7 +24,7 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export function RoleSelection() {
-  usePageTitle("Inscription - Mobilic");
+  usePageTitle("Sélection de rôle - Mobilic");
   const classes = useStyles();
 
   return [
