39eb6d15e9077b8b7ac67d8e82da06ddf8b38423
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2024-04-02 14:48:45 +0200

    fix(css): use same card width for login & signup selection

--- web/common/RoleCard.js
+++ web/common/RoleCard.js
@@ -1,8 +1,24 @@
 import React from "react";
+import classNames from "classnames";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    [theme.breakpoints.up("md")]: {
+      minWidth: "360px"
+    }
+  }
+}));
 
 export function RoleCard({ destination, title, description, image }) {
+  const classes = useStyles();
   return (
-    <div className="fr-card fr-enlarge-link fr-col-12 fr-col-md-4">
+    <div
+      className={classNames(
+        "fr-card fr-enlarge-link fr-col-12 fr-col-md-4",
+        classes.card
+      )}
+    >
       <div className="fr-card__body">
         <div className="fr-card__content">
           <h4 className="fr-card__title">
