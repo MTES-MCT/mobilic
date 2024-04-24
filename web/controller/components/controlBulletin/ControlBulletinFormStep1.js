a591af8f5df4910ac07cfc75bdc4a926c67927b4
Gaspard "Sarp" Lonchampt <71872843+gaspard-lonchampt@users.noreply.github.com>
2024-04-24 09:53:52 +0200

    feat(controller): 3 input fields for user birth date in BDC (#494)
    * feat(controller): birth date BDC

--- web/controller/components/controlBulletin/ControlBulletinFormStep1.js
+++ web/controller/components/controlBulletin/ControlBulletinFormStep1.js
@@ -1,12 +1,17 @@
 import React, { useMemo } from "react";
 
 import Stack from "@mui/material/Stack";
-import { Select, TextInput } from "@dataesr/react-dsfr";
+import { Col, Row, Select, TextInput } from "@dataesr/react-dsfr";
 import { COUNTRIES } from "../../utils/country";
 import { DEPARTMENTS } from "../../utils/departments";
 import { useApi } from "common/utils/api";
 import { CONTROL_LOCATION_QUERY } from "common/utils/apiQueries";
 import { DsfrAutocomplete } from "../utils/DsfrAutocomplete";
+import { Box } from "@mui/material";
+import { CURRENT_YEAR } from "common/utils/time";
+
+const BIRTH_DATE_MIN_YEAR = 100;
+const BIRTH_DATE_MAX_YEAR = 18;
 
 export function ControlBulletinFormStep1({
   handleEditControlBulletin,
@@ -16,6 +21,51 @@ export function ControlBulletinFormStep1({
   const [departmentLocations, setDepartmentLocations] = React.useState([]);
 
   const api = useApi();
+  const [day, setDay] = React.useState(1);
+  const [month, setMonth] = React.useState(1);
+  const [year, setYear] = React.useState(1980);
+
+  React.useEffect(() => {
+    const { userBirthDate } = controlBulletin;
+    if (!userBirthDate) {
+      return;
+    }
+    const date = new Date(userBirthDate);
+    setYear(date.getFullYear());
+    setMonth(date.getMonth() + 1);
+    setDay(date.getDate());
+  }, [controlBulletin]);
+
+  const MAX_BIRTH_DATE_YEAR = React.useMemo(
+    () => CURRENT_YEAR - BIRTH_DATE_MAX_YEAR,
+    [CURRENT_YEAR]
+  );
+  const MIN_BIRTH_DATE_YEAR = React.useMemo(
+    () => CURRENT_YEAR - BIRTH_DATE_MIN_YEAR,
+    [CURRENT_YEAR]
+  );
+
+  const onValidateBirthDate = () => {
+    const validYear = Math.max(
+      MIN_BIRTH_DATE_YEAR,
+      Math.min(year, MAX_BIRTH_DATE_YEAR)
+    );
+    const validMonth = Math.max(0, Math.min(month - 1, 11));
+    const maxDay = validMonth === 1 ? 29 : 30;
+    const validDay = Math.max(1, Math.min(parseInt(day), maxDay));
+    const date = new Date(validYear, validMonth, validDay, 10, 0, 0, 0);
+    if (!isNaN(date)) {
+      const newDateString = date.toISOString().split("T")[0];
+      editControlBulletinField(newDateString, "userBirthDate");
+    }
+  };
+
+  const onChangeNDigits = (e, setter, n) => {
+    const firstTwoChars = e.target.value.slice(0, n);
+    if (!isNaN(firstTwoChars)) {
+      setter(firstTwoChars);
+    }
+  };
 
   React.useEffect(async () => {
     if (controlBulletin.locationDepartment) {
@@ -106,7 +156,9 @@ export function ControlBulletinFormStep1({
         onChange={e => handleEditControlBulletin(e)}
         label="Nom du salarié"
         required
-        messageType={!controlBulletin.userLastName && showErrors ? "error" : ""}
+        messageType={
+          !controlBulletin.userLastName && showErrors ? "error" : undefined
+        }
       />
       <TextInput
         value={controlBulletin.userFirstName || ""}
@@ -115,20 +167,47 @@ export function ControlBulletinFormStep1({
         label="Prénom du salarié"
         required
         messageType={
-          !controlBulletin.userFirstName && showErrors ? "error" : ""
+          !controlBulletin.userFirstName && showErrors ? "error" : undefined
         }
       />
-      <TextInput
-        value={controlBulletin.userBirthDate || ""}
-        name="userBirthDate"
-        onChange={e => handleEditControlBulletin(e)}
-        label="Date de naissance du salarié"
-        required
-        type="date"
-        messageType={
-          !controlBulletin.userBirthDate && showErrors ? "error" : ""
-        }
-      />
+      <Box sx={{ marginBottom: 4, maxWidth: "400px" }}>
+        <label className="fr-label">Date de naissance du salarié</label>
+        <Row gutters>
+          <Col n="3">
+            <TextInput
+              required
+              value={day}
+              inputMode="numeric"
+              onChange={e => onChangeNDigits(e, setDay, 2)}
+              onBlur={onValidateBirthDate}
+              label="Jour"
+              hint="Exemple: 14"
+            />
+          </Col>
+          <Col n="3">
+            <TextInput
+              inputMode="numeric"
+              required
+              value={month}
+              onChange={e => onChangeNDigits(e, setMonth, 2)}
+              onBlur={onValidateBirthDate}
+              label="Mois"
+              hint="Exemple: 12"
+            />
+          </Col>
+          <Col n="4">
+            <TextInput
+              inputMode="numeric"
+              required
+              value={year}
+              onChange={e => onChangeNDigits(e, setYear, 4)}
+              onBlur={onValidateBirthDate}
+              label="Année"
+              hint="Exemple: 1984"
+            />
+          </Col>
+        </Row>
+      </Box>
       <Select
         label="Nationalité du salarié"
         selected={controlBulletin.userNationality}
@@ -139,7 +218,7 @@ export function ControlBulletinFormStep1({
         }}
         options={COUNTRIES}
         messageType={
-          !controlBulletin.userNationality && showErrors ? "error" : ""
+          !controlBulletin.userNationality && showErrors ? "error" : undefined
         }
       />
     </Stack>
