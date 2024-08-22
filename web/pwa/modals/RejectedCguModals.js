36ab8899b577e4b14565f10223c51a5f4be43d4c
Gaspard Lonchampt <gaspard.lonchampt@beta.gouv.fr>
2024-08-22 17:55:09 +0200

    feat(data_export): full data history export when deleting account

--- web/pwa/modals/RejectedCguModals.js
+++ web/pwa/modals/RejectedCguModals.js
@@ -1,13 +1,58 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import Typography from "@mui/material/Typography";
 import { Button, Link } from "@dataesr/react-dsfr";
 import Modal, { modalStyles } from "../../common/Modal";
 import { prettyFormatDay } from "common/utils/time";
 import { useIsAdmin } from "../../common/hooks/useIsAdmin";
+import { useApi } from "common/utils/api";
+import { useSnackbarAlerts } from "../../common/Snackbar";
+import { useMatomo } from "@datapunt/matomo-tracker-react";
+import { HTTP_QUERIES } from "common/utils/apiQueries";
 
-export default function RejectedCguModal({ expiryDate, onRevert }) {
+export default function RejectedCguModal({ expiryDate, onRevert, userId }) {
   const { isAdmin } = useIsAdmin();
   const classes = modalStyles();
+  const api = useApi();
+  const alerts = useSnackbarAlerts();
+  const { trackLink } = useMatomo();
+
+  const downloadUserData = async () => {
+    try {
+      const options = {
+        // for testing purpose
+        // company_ids: [85],
+        user_id: userId,
+        ensure_full_date_range: true
+      };
+
+      trackLink({
+        href: `/companies/download_activity_report`,
+        linkType: "download"
+      });
+
+      const data = await api.jsonHttpQuery(HTTP_QUERIES.excelExport, {
+        json: options
+      });
+
+      if (data.result !== "ok") {
+        throw new Error("Failed to request data export");
+      }
+
+      alerts.success(
+        "Votre demande a été prise en compte. Le fichier sera envoyé par email d'ici quelques minutes.",
+        null,
+        9000
+      );
+    } catch (error) {
+      alerts.error(
+        "Erreur lors de la demande d'exportation des données.",
+        null,
+        9000
+      );
+      console.error("Error requesting data export:", error);
+    }
+  };
+
   return (
     <Modal
       open={true}
@@ -56,7 +101,7 @@ export default function RejectedCguModal({ expiryDate, onRevert }) {
       actions={
         <>
           <Button
-            onClick={() => {}}
+            onClick={downloadUserData}
             icon="fr-icon-download-line"
             iconPosition="left"
           >
