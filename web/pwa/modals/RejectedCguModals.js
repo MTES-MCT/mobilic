381f15fbd14ae87e793d18e006a04dd5abe75ed8
Gaspard "Sarp" Lonchampt <71872843+gaspard-lonchampt@users.noreply.github.com>
2024-09-05 15:57:19 +0200

    feat(data_export): full data history export when deleting account (#555)
    * feat(data_export): full data history export when deleting account
    * can't click twice on download button

--- web/pwa/modals/RejectedCguModals.js
+++ web/pwa/modals/RejectedCguModals.js
@@ -1,13 +1,48 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import Typography from "@mui/material/Typography";
 import { Button, Link } from "@dataesr/react-dsfr";
 import Modal, { modalStyles } from "../../common/Modal";
 import { prettyFormatDay } from "common/utils/time";
 import { useIsAdmin } from "../../common/hooks/useIsAdmin";
+import { useApi } from "common/utils/api";
+import { useSnackbarAlerts } from "../../common/Snackbar";
+import { useMatomo } from "@datapunt/matomo-tracker-react";
+import { HTTP_QUERIES } from "common/utils/apiQueries";
+import { formatApiError } from "common/utils/errors";
 
-export default function RejectedCguModal({ expiryDate, onRevert }) {
+export default function RejectedCguModal({ expiryDate, onRevert, userId }) {
   const { isAdmin } = useIsAdmin();
   const classes = modalStyles();
+  const api = useApi();
+  const alerts = useSnackbarAlerts();
+  const { trackLink } = useMatomo();
+  const [isEnabledDownload, setIsEnabledDownload] = React.useState(true);
+
+  const downloadUserData = async () => {
+    setIsEnabledDownload(false);
+    trackLink({
+      href: `/users/download_full_data_when_CGU_refused`,
+      linkType: "download"
+    });
+
+    try {
+      await api.jsonHttpQuery(HTTP_QUERIES.downloadFullDataWhenCGUrefused, {
+        json: { user_id: userId }
+      });
+      alerts.success(
+        "Votre demande a été prise en compte. Le fichier sera envoyé par email d'ici quelques minutes.",
+        null,
+        9000
+      );
+    } catch (err) {
+      alerts.error(
+        formatApiError(err),
+        "download_full_data_when_CGU_refused",
+        6000
+      );
+    }
+  };
+
   return (
     <Modal
       open={true}
@@ -56,9 +91,10 @@ export default function RejectedCguModal({ expiryDate, onRevert }) {
       actions={
         <>
           <Button
-            onClick={() => {}}
+            onClick={downloadUserData}
             icon="fr-icon-download-line"
             iconPosition="left"
+            disabled={!isEnabledDownload}
           >
             Télécharger l'ensemble des données
           </Button>
