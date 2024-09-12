983eef5e2c416c9c2e2e6fea526cdd65295436f5
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2024-09-12 15:33:17 +0200

    chore: upgrade react from 17 to 18

--- web/index.js
+++ web/index.js
@@ -1,8 +1,8 @@
 import "core-js";
-import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import React from "react";
 import Root from "./root";
+import { createRoot } from "react-dom/client";
 import { initSentry } from "common/utils/sentry";
 import { Crisp } from "crisp-sdk-web";
 
@@ -14,7 +14,9 @@ if (process.env.REACT_APP_CRISP_WEBSITE_ID) {
   });
 }
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<Root />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
