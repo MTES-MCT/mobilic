510202caaf65494d453ca2500e2bfe273b5f68ef
Tristan Gueguen <tristan.gueguen@gmail.com>
2024-11-05 16:32:08 +0100

    fix(semantics): remove className and style props as they are redundant with otherProps

--- web/common/semantics/Main.js
+++ web/common/semantics/Main.js
@@ -1,21 +1,12 @@
 import React from "react";
 import Container from "@mui/material/Container";
 
-export const Main = ({
-  className,
-  maxWidth = "xl",
-  sx = {},
-  style = {},
-  children,
-  ...otherProps
-}) => (
+export const Main = ({ maxWidth = "xl", sx = {}, children, ...otherProps }) => (
   <Container
     component="main"
     role="main"
     id="content"
-    className={className}
     maxWidth={maxWidth}
-    style={style}
     sx={{
       ...sx,
       padding: { xs: 0, md: "initial" }
