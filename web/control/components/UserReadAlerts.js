543b1a447877762a09e31f202f61bf8148d3ec8b
Tristan Gueguen <tristan.gueguen@gmail.com>
2024-12-19 12:17:52 +0100

    fix(infractions): update infractions mobile -> no weird horizontal scrolling

--- web/control/components/UserReadAlerts.js
+++ web/control/components/UserReadAlerts.js
@@ -49,7 +49,8 @@ const useStyles = makeStyles(theme => ({
     background: "white",
     paddingLeft: theme.spacing(2),
     paddingRight: theme.spacing(2),
-    zIndex: 300
+    zIndex: 300,
+    width: "100%"
   }
 }));
 
