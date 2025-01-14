10b054765b77a5d56f3c19404bb337d523d8fed2
Tristan Gueguen <tristan.gueguen@gmail.com>
2025-01-14 12:48:33 +0100

    fix(natinfs): NATINF 25666 is also to be displayed first

--- web/control/utils/sanctionComparator.js
+++ web/control/utils/sanctionComparator.js
@@ -1,6 +1,8 @@
+const NO_LIC_NATINFS = ["NATINF 23103", "NATINF 25666"];
+
 export const sanctionComparator = (alert1, alert2) => {
   // Display "absence de LIC" first
-  if (alert1.sanction === "NATINF 23103") return -1;
-  if (alert2.sanction === "NATINF 23103") return 1;
+  if (NO_LIC_NATINFS.includes(alert1.sanction)) return -1;
+  if (NO_LIC_NATINFS.includes(alert2.sanction)) return 1;
   return alert1.sanction.localeCompare(alert2.sanction);
 };
