4ec9a556a296050f2e5a63400e48f91991d9baa7
Tristan Gueguen <tristan.gueguen@gmail.com>
2025-05-12 19:41:52 +0200

    feat(auto-validations): new mission validation info notices

--- web/pwa/components/MissionValidations.js
+++ web/pwa/components/MissionValidations.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Stack } from "@mui/material";
 import { Accordion } from "@codegouvfr/react-dsfr/Accordion";
 import { formatDay } from "common/utils/time";
+import Notice from "../../common/Notice";
 
 const EmployeeValidation = validation => {
   const label = validation.isAuto
@@ -10,30 +11,85 @@ const EmployeeValidation = validation => {
         true
       )}`
     : `Validée par le salarié le ${formatDay(validation.receptionTime, true)}`;
-  return (
+
+  const description = validation.isAuto
+    ? "La validation automatique s'effectue 24 heures après le début de la mission si le salarié ne l'a pas validée avant."
+    : "";
+
+  return description ? (
     <Accordion label={label} className="success">
-      {validation.isAuto &&
-        "La validation automatique s'effectue 24 heures après le début de la mission si le salarié ne l'a pas validée avant."}
+      {description}
     </Accordion>
+  ) : (
+    <Notice title={label} type="success" lessPadding withBorders />
   );
 };
 
 const NoEmployeeValidation = () => (
+  <Notice
+    title="Validation par le salarié en attente"
+    type="warning"
+    withBorders
+    lessPadding
+  />
+);
+
+const AdminAutoValidation = validation => (
   <Accordion
-    label="Validation par le salarié en attente"
-    className="error"
-  ></Accordion>
+    label={`Validée automatiquement à la place du gestionnaire le ${formatDay(
+      validation.receptionTime,
+      true
+    )}`}
+    className="success"
+  >
+    La validation automatique s'effectue au plus tard 2 jours ouvrés après la
+    validation du salarié.
+  </Accordion>
+);
+const AdminManualValidation = validation => (
+  <Notice
+    title={`Validée par le gestionnaire le ${formatDay(
+      validation.receptionTime,
+      true
+    )}`}
+    type="success"
+    lessPadding
+    withBorders
+  />
+);
+
+const NoAdminValidation = () => (
+  <Notice
+    title="Validation par un gestionnaire en attente"
+    type="warning"
+    withBorders
+    lessPadding
+  />
 );
 
 export const MissionValidations = ({ mission }) => {
+  const doNotDisplayValidations = mission.isDeleted && !mission.complete;
+
+  if (doNotDisplayValidations) {
+    return;
+  }
   const employeeValidation = mission.validations.find(
     validation => !validation.isAdmin
+  );
+  const adminAutoValidation = mission.validations.find(
+    validation => validation.isAdmin && validation.isAuto
+  );
+  const adminManualValidation = mission.validations.find(
+    validation => validation.isAdmin && !validation.isAuto
   );
   return (
     <Stack direction="column" textAlign="left">
       {employeeValidation
         ? EmployeeValidation(employeeValidation)
         : NoEmployeeValidation()}
+      {adminAutoValidation && AdminAutoValidation(adminAutoValidation)}
+      {adminManualValidation && AdminManualValidation(adminManualValidation)}
+      {!adminAutoValidation && !adminManualValidation && NoAdminValidation()}
     </Stack>
   );
 };
