0f0ec5ef9b68b96d22b7f87dfc9e0f1edfb82544
Tristan Gueguen <tristan.gueguen@gmail.com>
2025-10-30 17:33:36 +0100

    feat(exports): exports banner + modal call

---
+++ web/admin/components/ExportsBanner.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { CircularProgress, Stack } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { fr } from "@codegouvfr/react-dsfr";
+import { Button } from "@codegouvfr/react-dsfr/Button";
+import { useExportsContext } from "../utils/contextExports";
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    backgroundColor: fr.colors.decisions.background.default.grey.hover,
+    color: fr.colors.decisions.background.flat.grey.default
+  }
+}));
+
+export const ExportsBanner = () => {
+  const classes = useStyles();
+  const { cancelExports, nbExports } = useExportsContext();
+
+  if (!nbExports) {
+    return;
+  }
+  return (
+    <Stack
+      direction="row"
+      className={classes.container}
+      px={4}
+      py={1}
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Stack direction="row" columnGap={1} alignItems="center">
+        <CircularProgress color="inherit" size="1rem" />
+        <div>{`${nbExports} export d’activité en cours de préparation...`}</div>
+      </Stack>
+      <Button
+        priority="tertiary no outline"
+        iconPosition="right"
+        iconId="fr-icon-close-line"
+        size="small"
+        onClick={cancelExports}
+      >
+        Annuler
+      </Button>
+    </Stack>
+  );
+};
