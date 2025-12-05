c693fc22b4f30dc0fc773842036ed8b9f09da2d6
Tristan Gueguen <tristan.gueguen@gmail.com>
2025-12-05 13:55:08 +0100

    chore(babel): node 22 ?

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,3 @@
-require("@babel/register")({
-  extensions: [".js", ".jsx"]
-});
-
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = "production";
 process.env.NODE_ENV = "production";
