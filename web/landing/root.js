6fd7f7145a91a09d67f7591a6906801025364b05
tristan-gueguen <tristan.gueguen@gmail.com>
2026-02-18 12:11:59 +0100

    feat(landing): delete old sections, rework about us section

--- web/landing/root.js
+++ web/landing/root.js
@@ -1,17 +1,12 @@
 import React from "react";
 import { Header } from "../common/Header";
 import { Footer } from "./footer";
-import { LandingSectionList } from "./sections/LandingSection";
 import { WebinarListSection } from "./sections/WebinarListSection";
 import { IntroSection } from "./sections/IntroSection";
 import { TalkingAboutUsSection } from "./sections/TalkingAboutUsSection";
 import { usePageTitle } from "../common/UsePageTitle";
-import Stack from "@mui/material/Stack";
 import { VideoSection } from "./sections/VideoSection";
-import { WhoSection } from "./sections/WhoSection";
-import { WhatToKnowSection } from "./sections/WhatToKnowSection";
 import { Main } from "../common/semantics/Main";
-import { Container } from "@mui/material";
 import { LoginBanner } from "./components/LoginBanner";
 import { AdminSection } from "./sections/AdminSection";
 import { SoftwareSection } from "./sections/SoftwareSection";
@@ -29,17 +24,7 @@ export const Landing = () => {
         <AdminSection />
         <SoftwareSection />
         {process.env.REACT_APP_FETCH_WEBINARS && <WebinarListSection />}
-        {/* <Container maxWidth="lg">
-          <Stack direction="column" gap={8} sx={{ marginY: 4 }}>
-            <LandingSectionList>
-              <VideoSection />
-              {process.env.REACT_APP_FETCH_WEBINARS && <WebinarListSection />}
-              <WhoSection />
-              <WhatToKnowSection />
-              <TalkingAboutUsSection />
-            </LandingSectionList>
-          </Stack>
-        </Container> */}
+        <TalkingAboutUsSection />
       </Main>
       <Footer />
     </>
