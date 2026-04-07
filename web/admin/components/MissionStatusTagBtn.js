516806c13ae33c169f268a0219537cf036923da3
Redwane <56769614+rzafari42@users.noreply.github.com>
2026-04-07 11:55:23 +0200

    chore(admin): add prop validation and optional chaining to mission status tag button

--- web/admin/components/MissionStatusTagBtn.js
+++ web/admin/components/MissionStatusTagBtn.js
@@ -4,13 +4,6 @@ import { useMatomo } from "@datapunt/matomo-tracker-react";
 import { Link } from "../../common/LinkButton";
 const { OPEN_MISSION_DRAWER_IN_ACTIVITY_PANEL } = require("common/utils/matomoTags");
 
-
-MissionStatusTagBtn.propTypes = {
-    children: PropTypes.node,
-    openMission: PropTypes.func,
-    missionId: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
-};
-
 export const MissionStatusTagBtn = ({ children, openMission, missionId }) => {
     const { trackEvent } = useMatomo();
 
@@ -24,13 +17,22 @@ export const MissionStatusTagBtn = ({ children, openMission, missionId }) => {
             to="#"
             onClick={e => {
                 console.log("MissionStatusTagBtn clicked, missionId:", missionId);
-                console.log('openMission function:', openMission);
+                console.log("openMission function:", openMission);
                 e.stopPropagation();
-                openMission && trackEvent(OPEN_MISSION_DRAWER_IN_ACTIVITY_PANEL);
-                openMission && openMission(missionId);
+
+                if (openMission) {
+                    trackEvent(OPEN_MISSION_DRAWER_IN_ACTIVITY_PANEL);
+                    openMission(missionId);
+                }
             }}
         >
             {children}
         </Link>
     );
-}
+};
+
+MissionStatusTagBtn.propTypes = {
+    children: PropTypes.node,
+    openMission: PropTypes.func,
+    missionId: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+};
