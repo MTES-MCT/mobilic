2bb70708c0e00b4a899308dbcc8c77869707d31e
Gaspard Lonchampt <lonchamptgaspard@gmail.com>
2026-04-08 14:12:22 +0200

    fix(impersonation): intercept logout during impersonation
    When impersonated, navigating to /logout now calls StopImpersonation
    and redirects to /support/impersonation instead of logging out.

--- web/login/logout.js
+++ web/login/logout.js
@@ -2,14 +2,28 @@ import React from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import { useApi } from "common/utils/api";
 import { useLoadingScreen } from "common/utils/loading";
+import { useStoreSyncedWithLocalStorage } from "common/store/store";
+import { STOP_IMPERSONATION_MUTATION } from "common/utils/apiQueries/impersonation";
 
 export function Logout() {
   const api = useApi();
   const history = useHistory();
   const location = useLocation();
   const withLoadingScreen = useLoadingScreen();
+  const store = useStoreSyncedWithLocalStorage();
 
   async function _logout() {
+    const userInfo = store.userInfo();
+    if (userInfo?.isImpersonated) {
+      await api.graphQlMutate(
+        STOP_IMPERSONATION_MUTATION,
+        {},
+        { context: { nonPublicApi: true } }
+      );
+      await store.updateUserIdAndInfo();
+      history.replace("/support/impersonation");
+      return;
+    }
     const queryString = new URLSearchParams(location.search);
     const next = queryString.get("next");
     await api.logout({
