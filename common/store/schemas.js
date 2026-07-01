f4255f1848a66a88ba1235fcd2421e93016b4c0b
Redwane <56769614+rzafari42@users.noreply.github.com>
2026-07-01 14:46:19 +0200

    fix(store): replace parseInt with Number.parseInt in schemas

--- common/store/schemas.js
+++ common/store/schemas.js
@@ -3,11 +3,11 @@ import { Map, String, List } from "./types";
 // What is stored in local storage and how to read/write to it
 export const LOCAL_STORAGE_SCHEMA = {
   userId: {
-    deserialize: value => (value ? parseInt(value) : value),
+    deserialize: value => (value ? Number.parseInt(value) : value),
     serialize: String.serialize
   },
   controllerId: {
-    deserialize: value => (value ? parseInt(value) : value),
+    deserialize: value => (value ? Number.parseInt(value) : value),
     serialize: String.serialize
   },
   userInfo: Map,
@@ -22,23 +22,23 @@ export const LOCAL_STORAGE_SCHEMA = {
   vehicles: Map,
   regulationComputationsByDay: List,
   lastSelectedCompanyId: {
-    deserialize: value => (value ? parseInt(value) : value),
+    deserialize: value => (value ? Number.parseInt(value) : value),
     serialize: String.serialize
   }
 };
 
 export const NON_PERSISTENT_SCHEMA = {
   nextRequestId: {
-    deserialize: value => (value ? parseInt(value) : 1),
+    deserialize: value => (value ? Number.parseInt(value) : 1),
     serialize: String.serialize
   },
   nextEntityObjectId: {
-    deserialize: value => (value ? parseInt(value) : 1),
+    deserialize: value => (value ? Number.parseInt(value) : 1),
     serialize: String.serialize
   },
   identityMap: Map,
   nextRequestGroupId: {
-    deserialize: value => (value ? parseInt(value) : 1),
+    deserialize: value => (value ? Number.parseInt(value) : 1),
     serialize: String.serialize
   },
   pendingRequests: List
