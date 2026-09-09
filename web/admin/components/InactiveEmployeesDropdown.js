39a66b64ddf534f1c4de5dd7442dfd26cce12bc1
Redwane <56769614+rzafari42@users.noreply.github.com>
2026-09-09 12:56:16 +0200

    refactor(admin): dedupe "N'a jamais utilisé Mobilic" label

--- web/admin/components/InactiveEmployeesDropdown.js
+++ web/admin/components/InactiveEmployeesDropdown.js
@@ -8,6 +8,7 @@ import {
   unixToJSTimestamp
 } from "common/utils/time";
 import { DropdownMenu } from "common/components/DropdownMenu";
+import { NEVER_USED_MOBILIC_LABEL } from "common/utils/employeeStatus";
 
 const THRESHOLD_30_DAYS = DAY * 30;
 
@@ -48,7 +49,7 @@ const useStyles = makeStyles(() => ({
 }));
 
 function formatLastActiveAt(lastActiveAt) {
-  if (!lastActiveAt) return "N'a jamais utilisé Mobilic";
+  if (!lastActiveAt) return NEVER_USED_MOBILIC_LABEL;
   const date = new Date(unixToJSTimestamp(lastActiveAt));
   const dateStr = date.toLocaleDateString("fr-FR", {
     day: "2-digit",
